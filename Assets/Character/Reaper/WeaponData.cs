cd6ab7cd486b13ee4ec5ad5ef33ab8e6e3b8607f
dkyrian <kyrian.delaplace@ynov.com>
2025-04-02 14:54:29 +0200

    update code

---
+++ Assets/Character/Reaper/WeaponData.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewWeapon", menuName = "Weapons/Weapon")]
+public class WeaponData : ScriptableObject
+{
+    public string weaponName;
+    public float damageBase;
+    public Weapons.DamageType damageType;
+    public float range;
+    public float attackSpeed;
+    public bool isLootable;
+    public Weapons.Effect effect;
+    public Weapons.Rarity rarity;
+
+    // Spécifique aux armes à distance
+    public int ammo;
+    public float reloadSpeed;
+
+    // Spécifique aux armes utilisant du mana
+    public int manaConsumption;
+    public GameObject Model3D;
+}
