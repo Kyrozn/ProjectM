f6f7931924ec314668f161d2a68d9dd2eea27895
bchloe <chloe.barbanson@ynov.com>
2025-04-02 14:07:37 +0200

    merge

--- Assets/Scripts/Animate.cs
+++ Assets/Scripts/Animate.cs
@@ -5,28 +5,25 @@ using UnityEngine;
 public class Animate : MonoBehaviour
 {
     Animator PlayerAnimator;
-
-    // Variable pour stocker la position précédente
-    private Vector3 previousPosition;
-
-    void Awake()
-    {
-        // Récupérer l'Animator
+    void Awake() {
         PlayerAnimator = GetComponent<Animator>();
-
-        // Initialiser la position précédente
-        previousPosition = transform.position;
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // Calculer la distance parcourue depuis la dernière frame
-        float distanceMoved = (transform.position - previousPosition).magnitude;
-
-        // Mettre à jour l'animation en fonction du déplacement
-        PlayerAnimator.SetFloat("walk", distanceMoved > 0 ? 1f : 0f);
-
-        // Mettre à jour la position précédente pour la prochaine frame
-        previousPosition = transform.position;
+        if (Input.GetAxis("Horizontal") > 0.1f || Input.GetAxis("Horizontal") < -0.1f){
+            float value;
+            value = Input.GetAxis("Horizontal");
+            if (Input.GetAxis("Horizontal") < -0.1f) {
+                value = Input.GetAxis("Horizontal") * -1f;
+            }
+            PlayerAnimator.SetFloat("walk", value);
+        } else if (Input.GetAxis("Vertical") > 0.1f || Input.GetAxis("Vertical") < -0.1f){
+            PlayerAnimator.SetFloat("walk", Input.GetAxis("Vertical"));
+        } else {
+            PlayerAnimator.SetFloat("walk", 0f);
+        }
     }
 }
+
--- Assets/Scripts/Colors.cs
+++ Assets/Scripts/Colors.cs
@@ -5,22 +5,8 @@ using UnityEngine;
 public class Colors : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        Transform childTransform = transform.Find("Beta_Joints");
-        if (childTransform != null)
-        {
-            Renderer childRenderer = childTransform.GetComponent<Renderer>();
-
-            if (childRenderer != null)
-            {
-                childRenderer.material.color = Color.red;
-            }
-        }
-        else
-        {
-            Debug.LogError("Aucun enfant nommé 'Beta_Joints' trouvé !");
-        }
+    void Start(){
+        
     }
 
     // Update is called once per frame
